c27c3d6cfcfda55a339540229c897b23b23b2f97
bls <blair.strang@gmail.com>
2014-05-21 07:37:55 -0400

    Fix builds on OSX; clang is failing to compile python-snappy.

--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -23,5 +23,7 @@ echo "Installing system dependencies via homebrew..."
 brew install protobuf log4cpp snappy
 
 echo "Installing Python dependencies (requires root!)"
-sudo easy_install protobuf python-snappy
+sudo easy_install protobuf 
+DAMMIT="-Wno-error=unused-command-line-argument-hard-error-in-future"
+sudo ARCHFLAGS="$DAMMIT" easy_install python-snappy
 
