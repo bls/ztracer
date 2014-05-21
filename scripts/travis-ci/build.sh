4a90c2f8e7e96d993eb8c5b21503cd38f8382de6
bls <blair.strang@gmail.com>
2014-05-21 08:38:23 -0400

    Try adding both clang & gcc, to see if clang builds work on OSX.

--- scripts/travis-ci/build.sh
+++ scripts/travis-ci/build.sh
@@ -5,9 +5,9 @@ cd src
 # So the Apple workers for travis-ci seem to vary quite a lot
 # in terms of the way they are set up. We *need* to use clang
 # to build PIN tools on OSX.
-if [ $(uname -s) == "Darwin" ]; then
-    export CC=clang
-    export CXX=clang++
-fi
+#if [ $(uname -s) == "Darwin" ]; then
+#    export CC=clang
+#    export CXX=clang++
+#fi
 make
 
